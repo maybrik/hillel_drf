c28d405608f82d8d50f3e90f700dbc5c7cf0e81c
Kai <maybrik1912@gmail.com>
2021-10-22 14:36:23 +0300

    added serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,2 +1,15 @@
+from .models import Post, Comment
 
 from rest_framework import serializers
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'title', 'text', 'image']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'author', 'text', 'for_post']
